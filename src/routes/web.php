245f3346a91f8261096172b2ba4859bc14fc25e5
AleksiyD <50235593+AleksiyD@users.noreply.github.com>
2024-05-02 00:32:00 +0300

    added laravel ui and auth classes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +21,23 @@ Route::controller(IndexController::class)->group(function() {
     Route::get('/', 'index')->name('index');
     Route::post('/send-feedback-form', 'send_feedback_form')->name('send-feedback-form');
 });
+
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
+
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
+
+//     return redirect('/home');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
+
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
+
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
