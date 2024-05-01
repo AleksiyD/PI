68e8e78421d36c8fb5ef4426777627a24fa44bcb
AleksiyD <50235593+AleksiyD@users.noreply.github.com>
2024-05-01 06:10:14 +0300

    added research section

---
+++ src/resources/views/inc/index/research/list-group-item.blade.php
@@ -0,0 +1,3 @@
+<li class="list-group-item">
+	<a href="{{ $src }}">{{ $title }}</a>
+</li>
---
+++ src/resources/views/inc/index/research/section.blade.php
@@ -0,0 +1,61 @@
+<section class="probootstrap-section probootstrap-bg probootstrap-section-dark" style="background-image: url(img/hero_bg_bw_1.jpg)" data-stellar-background-ratio="0.5">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center section-heading probootstrap-animate" data-animate-effect="fadeIn">
+				<h2>Научно-исследовательские работы</h2>
+			</div>
+		</div>
+		<div class="row">
+			<ul class="list-group probootstrap-animate">
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Разработка рецептуры функционального печеночного паштета с пребиотическим действием (ВАК 2386 в редакции от 20.12.2022, включен с 28.12.2018)',
+					'src' => 'https://elibrary.ru/item.asp?id=50093342'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Актуальность и перспективы использования лактозо-лактулозного продукта в мясной промышленности (ВАК 838 в редакции от 01.02.2022, включен с 12.02.2019)',
+					'src' => 'https://elibrary.ru/item.asp?id=54070847'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Актуальность использования отечественной лактозы в мясной промышленности',
+					'src' => 'https://elibrary.ru/item.asp?id=48563719'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Перспективы использования отечественной лактулозы в мясной промышленности',
+					'src' => 'https://elibrary.ru/item.asp?id=48563708'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Нутриентносбалансированные мясорастительные продукты с молочной мелассой',
+					'src' => 'https://elibrary.ru/item.asp?id=50406660'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Разработка рецептур продуктов питания с высокой пищевой и биологической ценностью',
+					'src' => 'https://elibrary.ru/item.asp?id=54400623'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Качество, безопасность и профилактическая направленность нового печеночного паштета',
+					'src' => 'https://elibrary.ru/item.asp?id=54165225'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Компьютерное моделирование нутриентного состава поликомпонентных пищевых продуктов как способ их перевода в сегмент здорового питания',
+					'src' => 'https://elibrary.ru/item.asp?id=38238771'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Методология и научно-практические результаты моделирования рецептур и технологий пищевых продуктов, блюд и кулинарных изделий',
+					'src' => 'https://elibrary.ru/item.asp?id=21745811'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Computer technologies allow us to create food products of the future (Компьютерные технологии позволяют создавать продукты питания будущего)',
+					'src' => 'https://www.growkudos.com/publications/10.1063%25252F5.0151741/reader'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'Development of New Types of Minced Meat Products for Preventive and Personalized Nutrition (Разработка новых видов фарша для профилактического и персонализированного питания)',
+					'src' => 'https://link.springer.com/chapter/10.1007/978-3-030-96641-6_3'
+				])
+				@include('inc.index.research.list-group-item', [
+					'title' => 'MEAT AND PLANT PRODUCTS DESIGNING BASED ON THE MULTILEVEL MODELING METHOD (ПРОЕКТИРОВАНИЕ МЯСНЫХ И РАСТИТЕЛЬНЫХ ПРОДУКТОВ НА ОСНОВЕ МЕТОД МНОГОУРОВНЕВОГО МОДЕЛИРОВАНИЯ)',
+					'src' => 'https://keypublishing.org/jhed/jhed-volumes/jhed-volume-24-i-fpp-7-aleksander-a-borisenko-andrey-a-bratsikhin-ludmila-a-saricheva-aleksey-a-borisenko-gennady-v-slyusarev-evgeniy-a-chebotarev-2018-meat-and-plant-products-des/'
+				])
+			</ul>
+		</div>
+	</div>
+</section>
