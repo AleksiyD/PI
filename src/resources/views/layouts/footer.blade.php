3baeb951ecc7b88d473e27ec53024328b73ca7e6
AleksiyD <50235593+AleksiyD@users.noreply.github.com>
2024-05-01 07:59:04 +0300

    added section contacts in footer

--- src/resources/views/layouts/footer.blade.php
+++ src/resources/views/layouts/footer.blade.php
@@ -1,62 +1,20 @@
 <footer id="footer" class="probootstrap-footer probootstrap-bg">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 probootstrap-animate">
+            <div class="col-md-12 probootstrap-animate">
                 <div class="probootstrap-footer-widget">
-                    <h3>About Us</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro provident suscipit natus a
-                        cupiditate ab minus illum quaerat maxime inventore Ea consequatur consectetur hic provident
-                        dolor ab aliquam eveniet alias</p>
-                    <ul class="probootstrap-footer-social">
-                        <li><a href="#"><i class="icon-twitter"></i></a></li>
-                        <li><a href="#"><i class="icon-facebook"></i></a></li>
-                        <li><a href="#"><i class="icon-github"></i></a></li>
-                        <li><a href="#"><i class="icon-dribbble"></i></a></li>
-                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
-                        <li><a href="#"><i class="icon-youtube"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-md-4 probootstrap-animate">
-                <div class="probootstrap-footer-widget">
-                    <h3>Contact Info</h3>
+                    <h3>Контакты</h3>
                     <ul class="probootstrap-contact-info">
-                        <li><i class="icon-location2"></i> <span>198 West 21th Street, Suite 721 New York NY
-                                10016</span></li>
-                        <li><i class="icon-mail"></i><span>info@domain.com</span></li>
-                        <li><i class="icon-phone2"></i><span>+123 456 7890</span></li>
+                        <li><i class="icon-ungroup"></i> <span>ООО «ПИЩЕВОЙ ИНЖИНИРИНГ»</span></li>
+                        <li><i class="icon-mail"></i><a href="mailto:kgreseva@bk.ru">kgreseva@bk.ru</a></li>
+                        <li><i class="icon-phone2"></i><a href="tel:+7-988-863-02-32">+7-988-863-02-32</a></li>
                     </ul>
 
                 </div>
             </div>
-
-            <div class="col-md-4 probootstrap-animate">
-                <div class="probootstrap-footer-widget">
-                    <h3>Donation</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit omnis nam obcaecati placeat.
-                        Repellendus omnis in praesentium molestiae rem eligendi.</p>
-                    <p><a href="#" class="btn btn-primary">Donate Now</a></p>
-                </div>
-            </div>
-
-        </div>
-        <!-- END row -->
-
-    </div>
-
-    <div class="probootstrap-copyright">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 text-left">
-                    <p>&copy; 2017 <a href="https://uicookies.com/">uiCookies:Charity</a>. All Rights Reserved. Designed
-                        &amp; Developed with <i class="icon icon-heart"></i> by <a
-                            href="https://uicookies.com/">uicookies.com</a></p>
-                </div>
-                <div class="col-md-4 probootstrap-back-to-top">
-                    <p><a href="#" class="js-backtotop">Back to top <i class="icon-arrow-long-up"></i></a></p>
-                </div>
-            </div>
+						<div class="col-md-12 probootstrap-back-to-top">
+							<p><a href="#" class="js-backtotop">Вверх <i class="icon-arrow-long-up"></i></a></p>
+					</div>
         </div>
     </div>
 </footer>
