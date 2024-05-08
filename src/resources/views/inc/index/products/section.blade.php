65115671903661baef4326fd246f521ad92d03d4
AleksiyD <50235593+AleksiyD@users.noreply.github.com>
2024-05-08 16:38:26 +0300

    added new section products

---
+++ src/resources/views/inc/index/products/section.blade.php
@@ -0,0 +1,23 @@
+<section class="probootstrap-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center section-heading probootstrap-animate" data-animate-effect="fadeIn">
+                <h2>Выработанные продукты в соответствии с рецептурами</h2>
+            </div>
+        </div>
+        <div class="row product-container">
+            <div class="card-wrapper col-md-3 col-sm-12 col-xs-12 col-xxs-12 probootstrap-animate card-columns product-img-wrapper">
+                <img class="product-img" src="{{ asset('img/1_patent.jpg') }}" alt="">
+            </div>
+            <div class="card-wrapper col-md-3 col-sm-12 col-xs-12 col-xxs-12 probootstrap-animate card-columns product-img-wrapper">
+                <img class="product-img" src="{{ asset('img/2_patent.jpg') }}" alt="">
+            </div>
+            <div class="card-wrapper col-md-3 col-sm-12 col-xs-12 col-xxs-12 probootstrap-animate card-columns product-img-wrapper">
+                <img class="product-img" src="{{ asset('img/3_patent.jpg') }}" alt="">
+            </div>
+            <div class="card-wrapper col-md-3 col-sm-12 col-xs-12 col-xxs-12 probootstrap-animate card-columns product-img-wrapper">
+                <img class="product-img" src="{{ asset('img/4_patent.jpg') }}" alt="">
+            </div>
+        </div>
+    </div>
+</section>
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -9,6 +9,7 @@
 @include('inc.index.general-info.section')
 @include('inc.index.project.section')
 @include('inc.index.development.section')
+@include('inc.index.products.section')
 @include('inc.index.research.section')
 @include('inc.index.mass-media-about-us.section')
 @include('inc.index.support.section')
